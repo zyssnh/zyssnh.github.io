6d4f95b5c63b54e4fe87ec26c132dc0e62fb117d
zyssnh <1123611308@qq.com>
2026-01-14 23:19:00 +0800

    配置Astro 项目涉及 GitHub 页面

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    // base: '/',
     site: 'https://zyssnh.github.io',
-    base: '/',
+    // outDir: './dist',
 });
